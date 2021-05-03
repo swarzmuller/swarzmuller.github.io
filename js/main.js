1da85f50cba482733e7a0ab630acb40000e21276
Vitalii <vitalii.ilchenko@gmail.com>
2021-05-03 17:05:16 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const overlay = document.querySelector('.overlay');
+
+hamburger.addEventListener('click', function () {
+    menu.classList.add('active');
+    overlay.classList.add('active');
+});
+
+document.addEventListener('click', function (e) {
+    const target = e.target;
+    if (target.classList.contains('menu__close') || target.classList.contains('overlay')) {
+       menu.classList.remove('active');
+       overlay.classList.remove('active');
+   }
+});
